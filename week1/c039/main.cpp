d7dc937d250a1b505465c98a8ed11508c06cd0eb
bochain <bochainwu@gmail.com>
2024-02-24 20:55:12 +0800

    week c039 13ms 324KB

---
+++ week1/c039/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+    int i ,j;
+    while (cin>>i>>j)
+    {
+        int m = min(i, j);
+        int n = max(i, j);
+        int c = 0;
+
+        for (int k=m; k<=n; k++){
+            int n = k, t = 0;
+            while (n != 1)
+            {
+                t++;
+                if (n % 2) {
+                    n = n*3 + 1;
+                } else {
+                    n = n/2;
+                }
+            }
+            c = max(t, c);
+        }
+        cout<<i<<" "<<j<<" "<<c<<endl;
+    }
+    return 0;   
+}
